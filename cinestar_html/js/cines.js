839d6df2e86a08b469ed819aec176ed31ab2bcac
aaron <aaronlurita123@gmail.com>
2024-08-22 12:06:39 -0500

    js

---
+++ cinestar_html/js/cines.js
@@ -0,0 +1,31 @@
+
+
+const getcines = async ()=> {
+    const data = await fetch(`http://localhost/sweb/cinestar_sweb_php/cines`)
+    
+    if (data.status == 200){
+        const cines = await data.json()
+        let html =`<br/><h1>Nuestros Cines</h1><br/>`
+
+        cines.forEach(cine => {
+            html +=`
+            <div class="contenido-cine">
+	        	    <img src="img/cine/${cine.id}.1.jpg" width="227" height="170"/>
+            	   	<div class="datos-cine">
+       				   	<h4>${cine.RazonSocial}</h4><br/>
+                		<span>${cine.Direccion} - ${cine.Detalle}<br/><br/>Teléfono:${cine.Telefonos}</span>
+                	</div>
+                	<br/>
+                	<a href="cine.html?id_${cine.id}">
+                		<img src="img/varios/ico-info2.png" width="150" height="40"/>
+                	</a>
+				</div>
+            
+            `
+        });
+
+        document.getElementById('contenido-interno').innerHTML = html
+    }
+
+}
+getcines()
